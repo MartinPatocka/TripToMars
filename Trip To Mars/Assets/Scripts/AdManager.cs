33d2e005322cc05caa329ba6ec01813a553b81ce
Martin Patočka <patocka16@seznam.cz>
2020-11-03 21:05:19 +0100

    AddAdvertisement
    Adding advertisement
    Banner and RewardVideo

---
+++ Trip To Mars/Assets/Scripts/AdManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour, IUnityAdsListener
+{
+    private string playStoreID = "3889073";
+
+    private string interstitialAd = "video";
+    private string rewardedVideoAd = "rewardedVideo";
+
+    public bool isTargetPlayStore;
+    public bool isTestAd;
+
+    void Start()
+    {
+        Advertisement.AddListener(this);
+        InitializeAdvertisement();
+    }
+
+    private void InitializeAdvertisement()
+    {
+        if (isTargetPlayStore)
+        {
+            Advertisement.Initialize(playStoreID, isTestAd);
+            return;
+        }
+    }
+
+    public void PlayInterstitialAd()
+    {
+        if (!Advertisement.IsReady(interstitialAd))
+        {
+            return;
+        }
+        Advertisement.Show(interstitialAd);
+    }
+
+    public void PlayRewardedVideoAd()
+    {
+        if (!Advertisement.IsReady(rewardedVideoAd))
+        {
+            return;
+        }
+        Advertisement.Show(rewardedVideoAd);
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        switch (showResult)
+        {
+            case ShowResult.Failed:
+                break;
+            case ShowResult.Skipped:
+                break;
+            case ShowResult.Finished:
+                if (placementId == rewardedVideoAd) { Debug.Log("Reward The Player"); }
+                if (placementId == interstitialAd) { Debug.Log("Finished interstitial"); }
+                break;
+        }
+    }
+}
---
+++ Trip To Mars/Assets/Scripts/BannerAd.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAd : MonoBehaviour
+{
+    private string gameID = "3889073";
+    private string placementId = "banner";
+    public bool testMode = true;
+
+    IEnumerator Start()
+    {
+        Advertisement.Initialize(gameID, testMode);
+
+        while (!Advertisement.IsReady(placementId))
+            yield return null;
+
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Show();
+
+    }
+}
